2a598175ed0e01f8db81699d8a760316d0358b97
Arkopravo Ghosh <arkopravoghosh789.com>
2025-01-11 21:56:10 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View, Image, Button } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App Testing</Text>
+      <Image 
+        source={{uri:'https://cdn2.unrealengine.com/body-7-spotlight-mhp-pagani-1920x1080-397763522.jpg?resize=1&w=1920'}}
+        style={{width: 450, height: 250}}
+      />
+      
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
